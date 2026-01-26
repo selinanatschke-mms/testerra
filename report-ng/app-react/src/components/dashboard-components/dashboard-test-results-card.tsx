f33312402ed37ac86919124d1c3d6d24a05d73fa
sena <selina.natschke@t-systems.com>
2026-01-26 13:59:05 +0100

    create component button list

--- report-ng/app-react/src/components/dashboard-components/dashboard-test-results-card.tsx
+++ report-ng/app-react/src/components/dashboard-components/dashboard-test-results-card.tsx
@@ -1,36 +1,21 @@
-import {useTheme} from "@mui/material";
 import React from "react";
 import ReportCard from "../../widgets/report-card/report-card";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
+import ButtonList from "../../widgets/button-list/button-list";
 import CancelIcon from "@mui/icons-material/Cancel";
-import ListItemText from "@mui/material/ListItemText";
+import {useTheme} from "@mui/material";
+
 
 const DashboardTestResultsCard: React.FC = () => {
     const theme = useTheme();
 
+    const itemList = [
+        { primaryText: "1", secondaryText: "Failed", icon: <CancelIcon sx={{color: theme.custom.statusColors.failed}}/>},
+        { primaryText: "1", secondaryText: "Expected Failed", icon: <CancelIcon sx={{color: theme.custom.statusColors.expected_failed}}/>},
+    ]
+
     return (
         <ReportCard label="Tests: 7" sx={{p: 0, ":last-child": {padding: 0}}}>
-            <List sx={{p: 0}}>
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <CancelIcon sx={{color: theme.custom.statusColors.failed}}/>
-                        </ListItemIcon>
-                        <ListItemText primary="1" secondary="Failed"/>
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <CancelIcon sx={{color: theme.custom.statusColors.expected_failed}}/>
-                        </ListItemIcon>
-                        <ListItemText primary="1" secondary="Expected Failed"/>
-                    </ListItemButton>
-                </ListItem>
-            </List>
+            <ButtonList list={itemList}/>
         </ReportCard>
     );
 };
---
+++ report-ng/app-react/src/widgets/button-list/button-list.tsx
@@ -0,0 +1,42 @@
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import List from "@mui/material/List";
+import {Typography} from "@mui/material";
+
+type ListItem = {
+    primaryText: string,
+    secondaryText?: string,
+    icon: any,
+}
+
+type ButtonListProps = {
+    list: ListItem[],
+    disablePadding?: boolean
+}
+
+const ButtonList = ({list, disablePadding}: ButtonListProps) => {
+    return (
+        <List sx={!disablePadding ? {p: 0} : undefined}>
+            {list.map((item) => (
+                <ListItem disablePadding>
+                    <ListItemButton sx={disablePadding ? {pt: 0, pb: 0} : undefined}>
+                        <ListItemIcon>
+                            {item.icon}
+                        </ListItemIcon>
+
+                        {/* This ListItemText uses disabledTypography (to explicity forbid using the built in typography) to add a separate typography element as its child component
+                            The advantage of this is, that we can use the provided noWrap property which automatically truncates the text.*/}
+                        <ListItemText disableTypography={true}>
+                            <Typography noWrap>{item.primaryText}</Typography>
+                            <Typography color="primary" sx={{fontSize: 14}}>{item.secondaryText}</Typography>
+                        </ListItemText>
+
+                    </ListItemButton>
+                </ListItem>
+            ))}
+        </List>
+    )
+}
+export default ButtonList;
